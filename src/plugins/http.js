830f90a71904b184860317cf68a044216dbc562c
ziyuan <onbpazy@163.com>
2018-09-11 01:20:58 +0800

    配置

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -2,5 +2,6 @@ import Vue from "vue";
 import axios from "axios";
 
 axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 5000;
 
 Vue.prototype.$http = axios;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="home">
     <div>
-      <el-button type="primary" icon="el-icon-refresh" circle @click="load" :loading="loading"></el-button>
+      <el-button type="success" icon="el-icon-refresh" circle @click="load" :loading="refreshLoading"></el-button>
+      <el-button type="primary" icon="el-icon-plus" circle @click="showDialog" :loading="addLoading"></el-button>
     </div>
     <p v-for="item of list" :key="item.port">{{item.port}}({{item.alias}}): {{item.usage | readable}}</p>
+
+    <el-dialog title="SS流量监控配置" :visible.sync="addDialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="端口号" label-width="120px">
+          <el-input v-model="form.port" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="别名" label-width="120px">
+          <el-input v-model="form.alias" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="hideDialog">取 消</el-button>
+        <el-button type="primary" @click="confirm">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -13,17 +29,42 @@ export default {
   data() {
     return {
       list: [],
-      loading: false
+      form: {
+        port: "",
+        alias: ""
+      },
+      refreshLoading: false,
+      addLoading: false,
+      addDialogFormVisible: false
     };
   },
   async created() {
-    this.load();
+    await this.load();
   },
   methods: {
     async load() {
-      this.loading = true;
+      this.refreshLoading = true;
       this.list = (await this.$http.get("/list")).data;
-      this.loading = false;
+      this.refreshLoading = false;
+    },
+    async add() {
+      await this.$http.post("/save", {
+        port: parseInt(this.form.port),
+        alias: this.form.alias
+      });
+      await this.load();
+    },
+    async confirm() {
+      this.add();
+      this.hideDialog();
+    },
+    hideDialog() {
+      this.addDialogFormVisible = false;
+      this.addLoading = false;
+    },
+    showDialog() {
+      this.addLoading = true;
+      this.addDialogFormVisible = true;
     }
   },
   filters: {
