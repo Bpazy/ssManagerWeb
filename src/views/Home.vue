52572d2fc4c9ff37bc0379f3a3b5153ab5742368
ziyuan <onbpazy@163.com>
2018-09-11 21:07:00 +0800

    删除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div class="home">
     <div>
       <el-button type="success" icon="el-icon-refresh" circle @click="load" :loading="refreshLoading"></el-button>
-      <el-button type="primary" icon="el-icon-plus" circle @click="add" :loading="addLoading"></el-button>
+      <el-button type="primary" icon="el-icon-plus" circle @click="addRule" :loading="addLoading"></el-button>
     </div>
-    <p v-for="item of list" :key="item.port">{{item.port}}({{item.alias}}): {{item.usage | readable}}</p>
+
+    <div v-for="(rule, index) of rules" :key="rule.port" class="item">
+      <el-button type="primary" icon="el-icon-delete" size="mini" circle @click="deleteRule(rule, index)" :loading="rule.loading"></el-button>
+      <span class="itemDesc">{{rule.port}}({{rule.alias}}): {{rule.usage | readable}}</span>
+    </div>
   </div>
 </template>
 
@@ -13,26 +17,29 @@ export default {
   name: "home",
   data() {
     return {
-      list: [],
+      rules: [],
       form: {
         port: "",
         alias: ""
       },
       refreshLoading: false,
       addLoading: false,
+      deleteLoading: false,
       addDialogFormVisible: false
     };
   },
   async created() {
     await this.load();
+    this.rules.forEach(rule => (rule.loading = false));
   },
   methods: {
     async load() {
       this.refreshLoading = true;
-      this.list = (await this.$http.get("/list")).data;
+      this.rules = (await this.$http.get("/list")).data;
       this.refreshLoading = false;
     },
-    add() {
+    addRule() {
+      this.addLoading = true;
       const self = this;
       this.$dialog.open({
         component: () => import("@/views/AddSetting"),
@@ -48,12 +55,39 @@ export default {
             });
             await self.load();
             self.$dialog.close();
+            self.addLoading = false;
           },
           cancel() {
             self.$dialog.close();
+            self.addLoading = false;
           }
         }
       });
+    },
+    async deleteRule(rule, index) {
+      this.setRuleLoadingState(index, true);
+
+      try {
+        await this.$confirm("此操作将导致历史记录被清空, 是否继续?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        });
+        const { code, msg } = (await this.$http.get(
+          `/delete/${rule.port}`
+        )).data;
+        if (code === "Ok") this.$message({ message: msg, type: "success" });
+        else this.$message.error(msg);
+
+        await this.load();
+      } catch (e) {
+        this.setRuleLoadingState(index, false);
+      }
+    },
+    setRuleLoadingState(index, loadingState) {
+      const rule = this.rules[index];
+      rule.loading = loadingState;
+      this.$set(this.rules, index, rule);
     }
   },
   filters: {
@@ -63,3 +97,11 @@ export default {
   }
 };
 </script>
+<style scoped>
+.item {
+  margin: 5px;
+}
+.itemDesc {
+  margin: 0 5px 0 5px;
+}
+</style>
